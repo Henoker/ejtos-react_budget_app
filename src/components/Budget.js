16c9bd44bd21261faf53459c634a4a0aec58ddbd
Henoker <hennybany@gmail.com>
2024-05-09 12:00:04 -0400

    inish first task

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,3 +1,4 @@
+//Budget app
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
@@ -5,13 +6,20 @@ const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const inputValue = event.target.value;
+        if (!isNaN(inputValue) && inputValue <= budget) {
+            setNewBudget(inputValue);
+        } else {
+            alert(`The value cannot exceed remaining funds £${budget}`);
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
+
 export default Budget;
